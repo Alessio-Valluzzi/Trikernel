df27c04f530e78185fe8145d64ce7da82dbfeaaf
Alessio-Valluzzi <alessiovalluzzi2@gmail.com>
2026-07-20 14:01:35 -0200

    Screen bugs

--- kernel/screen.h
+++ kernel/screen.h
@@ -18,7 +18,7 @@ void set_cursor(int x, int y);
 
 void draw_line(int x0, int y0, int x1, int y1, uint32_t color);
 
-void draw_rectangle(int x, int y, int width, int height, uint32_t color, bool filled);
+void draw_rectangle(int x, int y, int width, int height, uint32_t color, booln filled);
 
 void draw_circle(int cx, int cy, int radius, uint32_t color);
 
